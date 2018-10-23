264c315cbd69876a216394139cdcb3159a9ff9ee
171970160@qq.com <171970160@qq.com>
2018-10-23 09:03:07 +0800

    edit readme.md

--- src/test/java/TestPOI.java
+++ src/test/java/TestPOI.java
@@ -22,26 +22,26 @@ public class TestPOI {
 
     @Test
     public void test1() throws IllegalAccessException, IntrospectionException, InvocationTargetException, IOException {
-        //导出参数配置
-        List<Col> cols = Lists.newArrayList(
-                new Col("姓名","name"),
-                new Col("年龄","age"),
-                new Col("生日","birthday"),
-                new Col("启用","active"),
-                new Col("创建日期","createDate",25),
-                new Col("统计",30,"B2:B1000+1"),
-                new Col("随机数",30,"int(RAND()*1000)")
-        );
-        TableParam tableParam=new TableParam(cols);
-        List<Person> list = new ArrayList<>();
-        list.add(new Person("小泽",30, LocalDate.of(1988,10,2),true, LocalDateTime.now()));
-        list.add(new Person("小苍",31, LocalDate.of(1987,1,2),true, LocalDateTime.now()));
-        list.add(new Person("吉泽",28, LocalDate.of(1988,10,  2),true, LocalDateTime.now()));
-        list.add(new Person("武藤",20, LocalDate.of(1980,12,2),true, LocalDateTime.now()));
-        list.add(new Person("波多",18, LocalDate.of(1990,8,15),true, LocalDateTime.now()));
-        Workbook workbook = ExcelUtils.excelExport(tableParam,list);
-
-        workbook.write(new FileOutputStream("D:\\"+LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))+".xlsx"));
-        System.out.println("OK");
+        ////导出参数配置
+        //List<Col> cols = Lists.newArrayList(
+        //        new Col("姓名","name"),
+        //        new Col("年龄","age"),
+        //        new Col("生日","birthday"),
+        //        new Col("启用","active"),
+        //        new Col("创建日期","createDate",25),
+        //        new Col("统计",30,"B2:B1000+1"),
+        //        new Col("随机数",30,"int(RAND()*1000)")
+        //);
+        //TableParam tableParam=new TableParam(cols);
+        //List<Person> list = new ArrayList<>();
+        //list.add(new Person("小泽",30, LocalDate.of(1988,10,2),true, LocalDateTime.now()));
+        //list.add(new Person("小苍",31, LocalDate.of(1987,1,2),true, LocalDateTime.now()));
+        //list.add(new Person("吉泽",28, LocalDate.of(1988,10,  2),true, LocalDateTime.now()));
+        //list.add(new Person("武藤",20, LocalDate.of(1980,12,2),true, LocalDateTime.now()));
+        //list.add(new Person("波多",18, LocalDate.of(1990,8,15),true, LocalDateTime.now()));
+        //Workbook workbook = ExcelUtils.excelExport(tableParam,list);
+        //
+        //workbook.write(new FileOutputStream("D:\\"+LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))+".xlsx"));
+        //System.out.println("OK");
     }
 }
